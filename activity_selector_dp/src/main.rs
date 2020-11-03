b2038f25d8591aaf88da04679a0ef7dd11942418
jleflang <jleflang@ucmerced.edu>
2020-11-02 17:44:24 -0800

    Better parsing of the file

--- activity_selector_dp/src/main.rs
+++ activity_selector_dp/src/main.rs
@@ -10,56 +10,74 @@ fn main() {
 
     let file = File::open("act.txt").expect("No File Found");
 
-    let buf = BufReader::new(file);
+    let mut buf = BufReader::new(file);
 
     let mut tasks = Vec::<l2s_algo::l2s_algo::Task>::new();
     let mut sets = Vec::new();
     let mut set = 0;
   
-    for line in buf.lines().skip(1) {
+    // Loop until the file is empty
+    loop {
 
-        // Consume the line
-        let l = line.expect("Line should not be empty");
+        let mut cur_str = String::new();
 
-        // Split the string
-        let unstruc = l.split_whitespace().collect::<Vec<&str>>();
+        // Get the header line
+        let line_size = buf.read_line(&mut cur_str).expect("Empty String");
 
-        match unstruc.len() {
-            1   => {
-                    // Store and ready for the next set
-                    if tasks.len() > 0 {
-                        sets.push(tasks.clone());
+        // EOF check
+        if line_size == 0 { break; }
 
-                        tasks.clear();
-                    }
-                },
-            3   => {  
-                    // Add the task
-                    let task = l2s_algo::l2s_algo::Task {
-                                id      : unstruc[0].parse::<u32>().unwrap(),
-                                start   : unstruc[1].parse::<u32>().unwrap(),
-                                end     : unstruc[2].parse::<u32>().unwrap(),
-                            };
+        // Set the number of jobs to schedule
+        let jobs = cur_str.trim().parse::<u32>().expect("Malformed Job Size");
 
-                    tasks.push(task);
-                },
-            _   => panic!("Malformed Read"), 
-        };
-                
+        // Clear the buffer
+        cur_str.clear();
+
+        for _ in 0..jobs {
+
+            // Read the next line
+            buf.read_line(&mut cur_str).expect("Empty String");
+
+            // Split the string
+            let job = cur_str.trim().split_whitespace().collect::<Vec<&str>>();
+
+            // If the string is not what we expect, panic
+            if job.len() != 3 { panic!("Malformed File")};
+
+            // Store the job
+            let task = l2s_algo::l2s_algo::Task {
+                        id      : job[0].parse::<u32>().unwrap(),
+                        start   : job[1].parse::<u32>().unwrap(),
+                        end     : job[2].parse::<u32>().unwrap(),
+            };
+
+            // Add to the list
+            tasks.push(task);
+
+            // Clear
+            cur_str.clear();
+
+        }
+
+        // Add to the list of sets
+        sets.push(tasks.clone());
+
+        // Clear the task list
+        tasks.clear();
     }
-
-    // Store the last thing
-    sets.push(tasks);
     
+    // Process each 
     for task in sets {
         // Increment the set number
         set += 1;
 
+        // Start the clock
         let start = Instant::now();
 
         // Run the algo
         let schedule = l2s_algo::l2s_algo::l2s_algo(task);
 
+        // Stop the clock
         let dur = start.elapsed();
 
         // Print out
