0b783d665216b2966232bb5a8ca6ea02e6a08fb2
jleflang <jleflang@ucmerced.edu>
2020-11-02 22:58:00 -0800

    More Efficient Rust

--- sorts/src/main.rs
+++ sorts/src/main.rs
@@ -3,7 +3,7 @@
 /// This is purely a standard academic exercise and thus may not be 
 /// optimal/correct for production use.
 use std::time::SystemTime;
-use rand::{thread_rng, Rng};
+use rand::{thread_rng, Rng, distributions::Uniform};
 
 fn main() {
 
@@ -17,52 +17,45 @@ fn main() {
 
     for i in size_pool.iter() {
 
-        // Create an empty array
-        let mut array: Vec<u32> = Vec::with_capacity(*i);
-
         // Get some RNG
         let mut rng = thread_rng();
+        let range = Uniform::from(0..10001);
 
-        // Fill the array
-        for _ in 0..array.capacity() {
-            array.push(rng.gen_range(0, 10000));
-        }
+        // Create & Fill the array
+        let array: Vec<u32> = (0..*i).map(|_| rng.sample(&range)).collect();
 
         // Run Insert Sort
         let mut start_time = SystemTime::now();
-        insert_sort(array.clone());
+        insert_sort(&mut array.clone());
         let since_sort_insert = start_time.elapsed();
 
         // Run Merge Sort
         start_time = SystemTime::now();
-        merge_sort(array.clone());
+        merge_sort(&mut array.clone());
         let since_sort_merge = start_time.elapsed();
 
         // Run Stooge Sort
         start_time = SystemTime::now();
-        stooge_sort(array.clone());
+        stooge_sort(&mut array.clone());
         let since_sort_stooge = start_time.elapsed();
 
         // Print the results
-        println!("{: <10}\t| {: <10.3?}\t| {: <10.3?}\t| {: <10.3?}", *i, 
-                                            since_sort_insert.unwrap(), 
-                                            since_sort_merge.unwrap(), 
-                                            since_sort_stooge.unwrap());
+        println!("{: <10}\t| {: <10.3?}\t| {: <10.3?}\t| {: <10.3?}", 
+                    *i, since_sort_insert.unwrap(), since_sort_merge.unwrap(), 
+                        since_sort_stooge.unwrap());
 
     }
 
 }
 
 /// Implementation of Insert Sort
-fn insert_sort(mut arr: Vec<u32>) {
+fn insert_sort(arr: &mut Vec<u32>) {
 
     for index in 1..arr.len() {
         let mut j = index;
 
         while (j > 0) && (arr[j - 1] < arr[j]) {
-            let tmp = arr[j];
-            arr[j] = arr[j - 1];
-            arr[j - 1] = tmp;
+            arr.swap(j, j - 1);
 
             j -= 1;
         }
@@ -71,15 +64,14 @@ fn insert_sort(mut arr: Vec<u32>) {
 }
 
 /// Implementation of Merge Sort
-fn merge_sort(mut arr: Vec<u32>) {
+fn merge_sort(arr: &mut Vec<u32>) {
 
-    let mid: usize = arr.len() / 2;
-    if mid == 0 {
-        return;
-    }
+    let mid = f32::floor(arr.len() as f32 / 2.0) as usize;
 
-    merge_sort(arr[..mid].to_vec());
-    merge_sort(arr[mid..].to_vec());
+    if mid == 0 { return; }
+
+    merge_sort(&mut arr[..mid].to_vec());
+    merge_sort(&mut arr[mid..].to_vec());
 
     let mut ret = arr.clone();
 
@@ -121,7 +113,7 @@ fn merge(left_arr: &[u32], right_arr: &[u32], ret: &mut [u32]) {
 }
 
 /// Implementation of Stooge Sort
-fn stooge_sort(mut arr: Vec<u32>) {
+fn stooge_sort(arr: &mut Vec<u32>) {
 
     let len = arr.len();
 
@@ -130,16 +122,14 @@ fn stooge_sort(mut arr: Vec<u32>) {
     }
 
     if arr[0] > arr[len - 1] {
-        let tmp = arr[0];
-        arr[0] = arr[len - 1];
-        arr[len - 1] = tmp; 
+        arr.swap(0, len - 1); 
     }
 
     if len > 2 {
-        let m = len / 3 + (len % 3 != 0) as usize;
+        let m =  f32::floor(len as f32 / 3.0) as usize;
 
-        stooge_sort(arr[..len-m].to_vec());
-        stooge_sort(arr[m..len].to_vec());
-        stooge_sort(arr[..len-m].to_vec());
+        stooge_sort(&mut arr[..len-m].to_vec());
+        stooge_sort(&mut arr[m..len].to_vec());
+        stooge_sort(&mut arr[..len-m].to_vec());
     }
 }
